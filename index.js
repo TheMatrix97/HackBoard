8f58dfdb318efbbbae3fe6d793b5364b06723d7b
marc catrisse <marc@catrisse.net>
2018-10-20 00:46:25 +0200

    primer test

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function (socket) {
+    io.emit('conexion','Alguien se ha conectado');
 });
-
-
