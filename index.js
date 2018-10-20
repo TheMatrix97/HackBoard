519dd379e7aab31bf870c69fcab22afbe66707a5
marc catrisse <marc@catrisse.net>
2018-10-20 03:30:40 +0200

    Working

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
+server.listen(80);
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/asd.html');
 });
+var line_history = [];
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function (socket) {
+    socket.on('update',function(data){
+      line_history.push(data);
+      console.log("update");
+      socket.broadcast.emit('refresh',data);
+    });
 });
-
-
